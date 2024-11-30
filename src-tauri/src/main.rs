bf6f04ec60a1cf69b39e6a1ba9f05f9c21b07a77
kittymoonsz <iamkittymoon@gmail.com>
2024-11-29 22:30:28 -0300

    tauri integration

---
+++ src-tauri/src/main.rs
@@ -0,0 +1,12 @@
+#[cfg(any(
+    target_os = "linux", 
+    target_os = "freebsd", 
+    target_os = "dragonfly", 
+    target_os = "openbsd", 
+    target_os = "netbsd" ))] 
+
+
+fn main() {
+    std::env::set_var("WEBKIT_DISABLE_COMPOSITING_MODE", "1");
+    tauri_app_lib::run()
+}
